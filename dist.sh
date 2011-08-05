b6114731a388f9b2d761374c434837e6a6058419
Austen Dicken <cvpcsm@gmail.com>
2011-08-04 23:51:25 -0500

    moving some stuff and modifying dists

--- dist.sh
+++ dist.sh
@@ -3,7 +3,7 @@
 distfiles="
 	conf
 	inc
-	README
+	README*
 	";
 
 distname="dokuwiki_auth_yubikey"
@@ -12,21 +12,4 @@ date="$(date +%Y%m%d)"
 zipname="${distname}-${date}.zip"
 if [ -e ${zipname} ] ; then rm ${zipname}; fi
 zip -r ${zipname} ${distfiles}
-
-tgzname="${distname}-${date}.tar.gz"
-if [ -e ${tgzname} ] ; then rm ${tgzname}; fi
-tar -zcvf ${tgzname} ${distfiles}
-
-tbzname="${distname}-${date}.tar.bz2"
-if [ -e ${tbzname} ] ; then rm ${tbzname}; fi
-tar -jcvf ${tbzname} ${distfiles}
-
-tlzname="${distname}-${date}.tar.lzma"
-if [ -e ${tlzname} ] ; then rm ${tlzname}; fi
-tar --lzma -cvf ${tlzname} ${distfiles}
-
-txzname="${distname}-${date}.tar.xz"
-if [ -e ${txzname} ] ; then rm ${txzname}; fi
-tar --xz -cvf ${txzname} ${distfiles}
-
-
+md5sum ${zipname} > ${zipname}.md5sum
